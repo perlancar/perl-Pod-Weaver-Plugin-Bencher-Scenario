08fdbff697704740ec5b2b83a7350cd34d0af738
perlancar (@pc-office) <perlancar@gmail.com>
2015-11-17 19:47:06 +0700

    Initial commit (unfinished)

---
+++ lib/Pod/Weaver/Plugin/Bencher/Scenario.pm
@@ -0,0 +1,65 @@
+package Pod::Weaver::Plugin::Bencher::Scenario;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use Moose;
+with 'Pod::Weaver::Role::AddTextToSection';
+with 'Pod::Weaver::Role::Section';
+
+use Bencher;
+
+sub _process_module {
+    no strict 'refs';
+
+    my ($self, $document, $input, $package) = @_;
+
+    my $filename = $input->{filename};
+
+    # XXX handle dynamically generated module (if there is such thing in the
+    # future)
+    local @INC = ("lib", @INC);
+
+    {
+        my $package_pm = $package;
+        $package_pm =~ s!::!/!g;
+        $package_pm .= ".pm";
+        require $package_pm;
+    }
+
+    my $scenario = Bencher::parse_scenario(
+        scenario => ${"$package\::scenario"});
+
+    $self->add_text_to_section($document, 'BENCHMARKED MODULES')
+        if @modules;
+
+    $self->log(["Generated POD for '%s'", $filename]);
+}
+
+sub weave_section {
+    my ($self, $document, $input) = @_;
+
+    my $filename = $input->{filename};
+
+    my $package;
+    if ($filename =~ m!^lib/(Bencher/Scenario/.+)\.pm$!) {
+        $package = $1;
+        $package =~ s!/!::!g;
+        $self->_process_module($document, $input, $package);
+    }
+}
+
+1;
+# ABSTRACT: Put various information from scenario into POD
+
+=for Pod::Coverage ^(weave_section)$
+
+=head1 SYNOPSIS
+
+In your C<weaver.ini>:
+
+ [-Bencher::Scenario]
+
+
+=head1 DESCRIPTION
